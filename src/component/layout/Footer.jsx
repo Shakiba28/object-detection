9cc80ee0e214acdb971430308d15d762bcf0a40e
shakiba <bshkyba430@gmail.com>
2024-05-09 21:35:11 +0330

    change src image

--- src/component/layout/Footer.jsx
+++ src/component/layout/Footer.jsx
@@ -14,7 +14,7 @@ export default function Footer() {
                 <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
                     <div className="flex sm:flex sm:items-center sm:justify-between">
                         <a href="#" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
-                            <img src="../../../public/logo.png" className="h-20" alt="Flowbite Logo" />
+                            <img src={"../../../public/logo.png"} className="h-20" alt="Flowbite Logo" />
                         </a>
                         <ul className="flex flex-wrap items-center mb-6 text-xl font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
                             <li className="mx-2">
